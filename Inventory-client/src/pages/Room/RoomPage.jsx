edc9dee8974f03f8c9a311031645ec41829e5874
Ertan Muslic <ertanmuslic@gmail.com>
2023-08-04 18:34:31 +0200

    Worker not loading problem fix

--- Inventory-client/src/pages/Room/RoomPage.jsx
+++ Inventory-client/src/pages/Room/RoomPage.jsx
@@ -11,50 +11,29 @@ const RoomPage = () => {
     const { id } = useParams();
     const [room, setRoom] = useState([]);
     const [inventory,setInventory] = useState([])
+    const [worker, setWorker] = useState([])
     const [imageList,setImageList] = useState({})
     const imageListRef = ref(storage, "images/react-porfolio.png")
 
 
-    const PrintButton = () => {
-      window.print();
-    }
+    // const PrintButton = () => {
+    //   window.print();
+    // }
 
     const getRoomById = async (id) => {
       try {
         const result = await api.get(`/api/Room/${id}`);
         const data = result.data;
         setRoom(data);
-        //console.log(data)
-      }
-      catch (error) {
-        console.log(error)
-      }
-    }
-
-    const getAllInventories = async () => {
-      try {
-        const result = await api.get("/api/Inventory");
-        const data = result.data;
-        setInventory(data);
-        //console.log(data)
-      }
-      catch (error) {
-        console.log(error)
-      }
-    }
-
-    const getWorkerById = async (id) => {
-      try {
-        const result = await api.get(`/api/Worker/${id}`);
-        const data = result.data;
-        //setWorker(data);
         console.log(data)
       }
       catch (error) {
         console.log(error)
       }
     }
-    
+
+   
+  
     useEffect(() =>{
 
       getDownloadURL(imageListRef).then((url) => {
@@ -70,14 +49,13 @@ const RoomPage = () => {
       // })
        
       getRoomById(id);
-      //getWorkerById(room.workerId)
-      getAllInventories();
     },[id])
 
 
-    
+    const boss = room.worker;
     const roomInventory = inventory.find(item => item.roomId == id);
-    if (!roomInventory) {
+    if (!roomInventory || !boss) {
+          getRoomById(id);
         return <div>Loading...</div>;
       }
 
@@ -98,12 +76,12 @@ const RoomPage = () => {
             <br />
             <h1>Inventar</h1>
             <div>
-              <h1>Name: {roomInventory.name}</h1>
-              <h1>Serial Number: {roomInventory.serialNumber}</h1>
-              <h1>Mark: {roomInventory.mark}</h1>
-              <h1>Model: {roomInventory.model}</h1>
-              <h1>Quantity: {roomInventory.quantity}</h1>
-              <h1>Price: {roomInventory.price}</h1>
+              <h1>Name: {room.inventory.name}</h1>
+              <h1>Serial Number: {room.inventory.serialNumber}</h1>
+              <h1>Mark: {room.inventory.mark}</h1>
+              <h1>Model: {room.inventory.model}</h1>
+              <h1>Quantity: {room.inventory.quantity}</h1>
+              <h1>Price: {room.inventory.price}</h1>
             </div>
             <br />
             <h1>Worker</h1>
