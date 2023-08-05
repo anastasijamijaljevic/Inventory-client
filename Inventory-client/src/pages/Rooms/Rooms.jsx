47fe1cdd62731a19569bdded2989d6ffc4430180
Anastasija <anastasijamijaljevic11@gmail.com>
2023-08-05 17:19:23 +0200

    rent a room

--- Inventory-client/src/pages/Rooms/Rooms.jsx
+++ Inventory-client/src/pages/Rooms/Rooms.jsx
@@ -3,28 +3,37 @@ import { useEffect } from 'react'
 import api from '../../api/api'
 import './Rooms.css'
 import { Link } from 'react-router-dom'
+import { useParams } from 'react-router-dom'
+import CreateRoom from '../../components/CreateRoom'
+
 
 
 const Rooms = () => {
+  const { id } = useParams();
   const [rooms, setRooms] = useState([]);
 
+  const refreshRooms = async () => {
+    try {
+      const result = await api.get('/api/Room');
+      const data = result.data;
+      setRooms(data);
+      console.log(data);
+    } catch (error) {
+      console.log('Error fetching rooms:', error);
+    }
+  };
+ 
   useEffect(() => {
 
-    const getAllRooms = async () => {
-      try {
-        const result = await api.get('/api/Room');
-        const data = result.data;
-        setRooms(data);
-        console.log(data);
-      } catch (error) {
-        console.log('Error fetching rooms:', error);
-      }
-    };
-
-    getAllRooms();
+    refreshRooms();
   }, []);
 
+  
+
+ 
+
   return (
+    <>
     <div className="room-list-container">
       <h1>Rooms</h1>
       <ul className="room-list">
@@ -48,7 +57,10 @@ const Rooms = () => {
         ))}
       </ul>
     </div>
+    <CreateRoom refreshRooms={refreshRooms} />
+  
 
+</>
   )
 
 }
