88854691f013b4c584d94b2e0e7f346f206135aa
Anastasija <anastasijamijaljevic11@gmail.com>
2023-08-06 20:42:48 +0200

    adding a navbar component

---
+++ Inventory-client/src/components/Navbar/Navbar.jsx
@@ -0,0 +1,22 @@
+import './Navbar.css'
+import { Link } from 'react-router-dom'
+
+const Navbar = () => {
+    return (
+        <nav className='navbar'>
+        <div className='nav-logo'></div>
+        <div className="nav-right">
+  <ul className="nav-list">
+    <li><Link to="/" className="nav-link">Home</Link></li>
+    <li> <Link to="/rooms" className="nav-link">Rooms</Link></li>
+    <li> <Link to="/howitworks" className="nav-link">How It Works</Link></li>
+    <li> <Link to="/about" className="nav-link">About Us</Link></li>
+    <li> <Link to="/login" className="nav-link">Log In</Link></li>
+    <li> <Link to="/register" className="nav-link">Sign Up</Link></li>
+  </ul>
+</div>
+    </nav>
+    )
+}
+
+export default Navbar
--- Inventory-client/src/pages/Home/Home.jsx
+++ Inventory-client/src/pages/Home/Home.jsx
@@ -1,23 +1,12 @@
-import { Link } from 'react-router-dom'
+
 import './Home.css'
+import Navbar from '../../components/Navbar/Navbar'
 
 
 const Home = () => {
     return (
         <section className='homepage'>
-            <nav className='navbar'>
-                <div className='nav-logo'></div>
-                <div className="nav-right">
-          <ul className="nav-list">
-            <li><Link to="/" className="nav-link">Home</Link></li>
-            <li> <Link to="/rooms" className="nav-link">Rooms</Link></li>
-            <li> <Link to="/howitworks" className="nav-link">How It Works</Link></li>
-            <li> <Link to="/about" className="nav-link">About Us</Link></li>
-            <li> <Link to="/login" className="nav-link">Log In</Link></li>
-            <li> <Link to="/register" className="nav-link">Sign Up</Link></li>
-          </ul>
-        </div>
-            </nav>
+          <Navbar/>
            
             <div className="left-section">
         <h1>Debt Inventory</h1>
